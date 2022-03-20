ada4c7671f439e09871ab517b4595a8e54356712
rafidmushfique <rafidmushfique@gmail.com>
2022-03-21 02:36:30 +0600

    Clear Message Added

--- index.js
+++ index.js
@@ -10,18 +10,21 @@ bot.on('ready', ()=>{
 console.log(process.env);
 APIKEY=process.env.API_KEY;
 TextChannelId=process.env.GENRAL_CHANNELID;
-bot.channels.cache.get(TextChannelId).send("afkBot Active!");
+bot.channels.cache.get(TextChannelId).send("Yes My Lord!");
 
 })
 bot.on('messageCreate', msg => {
     if(msg.channel.id==TextChannelId)
     {
-        var message=msg.content;
-        if(message=="!apex"|| message=="!Apex"){
-            var dm= msg.content.substring(1);
-            console.log(dm);
-            console.log("YES");
-            var apiurl='https://api.mozambiquehe.re/bridge?version=5&platform=PC&player=Rawglul&auth='+APIKEY;
+        var msgStr=msg.content;
+        var str=msgStr.split(" ");
+        var chckMessage=str[0];
+        var atParameter= str[1];
+        if(chckMessage=="!apex"|| chckMessage=="!Apex"){
+           
+            
+
+            var apiurl='https://api.mozambiquehe.re/bridge?version=5&platform=PC&player='+atParameter+'&auth='+APIKEY;
             axios({
                 method: 'get',
                 url: apiurl,
@@ -31,6 +34,7 @@ bot.on('messageCreate', msg => {
                     var Data=response.data;
                     var apexname=Data.global.name;
                     var brrank=Data.global.rank.rankName;
+                    var brrp=Data.global.rank.rankScore;
                     var brrankdiv=Data.global.rank.rankDiv;
                     var arenarank=Data.global.arena.rankName;
                     var arenadiv=Data.global.arena.rankDiv;
@@ -38,14 +42,31 @@ bot.on('messageCreate', msg => {
                     var currentStatus=Data.realtime.currentStateAsText;
                     bot.channels.cache.get(TextChannelId).send(
                       "Apex Name: "+apexname+"\n"
-                     +"Battley Royal Rank: "+brrank+"-"+brrankdiv+"\n"
-                     +"Arena Rank: "+arenarank+"-"+arenadiv+"\n"
+                     +"Battle Royal Rank: "+brrank+" "+brrankdiv+"\n"
+                     +"Battle Royal Rank Point: "+brrp+"\n"
+                     +"Arena Rank: "+arenarank+" "+arenadiv+"\n"
                      +"CurrentSelectedLegend: "+selectedLegend+"\n"
                      + "CurrentStatus: "+currentStatus +"\n"
                     );
-                });
+                })
+                .catch(function (error) {
+                    bot.channels.cache.get(TextChannelId).send("Player Doesnt Exists");
+                    console.log(error);
+                  });
           
      
+    }
+    else if(chckMessage=="!clear"){
+        var amount=100;
+        if(atParameter!=null)
+        {
+            var amount=atParameter;
+        }
+      
+    
+        msg.channel.bulkDelete(amount,true)
+    
+       
     }
 }
 })
